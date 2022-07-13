b309b8952bde11847768df6fabd0a5f6df8ac265
jerrychen <jerry@gmail.com>
2022-07-13 15:17:05 +0800

    完成敵方坦克跟牆面配置

--- src/main/java/tw/com/jerrycode/GameClient.java
+++ src/main/java/tw/com/jerrycode/GameClient.java
@@ -4,14 +4,19 @@ import javax.swing.*;
 
 import tw.com.jerrycode.gameobject.Direction;
 import tw.com.jerrycode.gameobject.Tank;
+import tw.com.jerrycode.gameobject.Wall;
+
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 
 public class GameClient extends JComponent {
     private int screenWidth;
     private int screenHeight;
 
     private Tank playerTank;
+    private ArrayList<Tank> enemyTanks = new ArrayList<Tank>();
+    private ArrayList<Wall> walls = new ArrayList<Wall>();
 
     GameClient() {
         this(800, 600);
@@ -22,6 +27,7 @@ public class GameClient extends JComponent {
         this.screenHeight = screenHeight;
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
 
+        // 遊戲初始
         init();
     }
 
@@ -38,9 +44,24 @@ public class GameClient extends JComponent {
         }).start();
     }
 
+    // 讀取圖形
     void init() {
-        playerTank = new Tank(250, 250, Direction.UP);
+        // 牆面圖形
+        Image wallImg = new ImageIcon("assets/images/brick.png").getImage();
+
+        // 玩家物件
+        playerTank = new Tank(380, 500, Direction.UP, false);
         playerTank.setSpeed(5);
+        // 產生敵方
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 7; j++) {
+                enemyTanks.add(new Tank(200 + j * 60, 50 + i * 60, Direction.DOWN, true));
+            }
+        }
+        // 牆面配置
+        walls.add(new Wall(wallImg, 80, 10, false, 15));
+        walls.add(new Wall(wallImg, 140, 10, true, 10));
+        walls.add(new Wall(wallImg, 640, 10, false, 15));
     }
 
     public int getScreenWidth() {
@@ -91,9 +112,15 @@ public class GameClient extends JComponent {
     protected void paintComponent(Graphics g) {
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, screenWidth, screenHeight);
-
+        // 繪製玩家
         playerTank.draw(g);
-
+        // 繪製敵方
+        for (Tank enemy : enemyTanks) {
+            enemy.draw(g);
+        }
+        // 繪製牆面
+        for (Wall wall : walls) {
+            wall.draw(g);
+        }
     }
-
 }
